e4be6b56bd3c0bc74baf95c09ba3205db662a8d3
Tahlia <tbolden13@gmail.com>
2022-10-26 22:08:52 -0700

    edited files

---
+++ EngineerInput.js
@@ -0,0 +1,18 @@
+
+class Engineer extends Employee {
+    constructor(name, id, email, githubUsername) {
+        
+        super(name, id, email);
+        this.githubUsername = githubUsername;
+    }
+
+    getGithub () {
+        return this.githubUsername;
+    }
+
+    getRole () {
+        return "Engineer";
+    }
+}
+
+module.exports = Engineer;
---
+++ InternInput.js
@@ -0,0 +1,15 @@
+class Intern extends Intern {
+    constructor(name, id, email, school) {
+
+        super(name, id, email);
+        this.school = school;
+    }
+    getSchool () {
+        return this.school;
+    }
+    getRole() {
+        return "Intern";
+    }
+}
+
+module.exports = Intern;
